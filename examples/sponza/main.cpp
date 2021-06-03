f8af7387a3b1a1e2582c5700acfc3c61488755c6
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-06-03 23:52:40 +0400

    Change OpenGL version from 3.3 to 4.0

--- examples/sponza/main.cpp
+++ examples/sponza/main.cpp
@@ -155,8 +155,8 @@ int main(void)
                 return INIT_ERR;
         }
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
