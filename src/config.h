36f0242b628308daafc032e56e9c56294bdfb98e
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-06-04 10:52:53 +0400

    Change root directory

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
 #pragma once
-#define LOVELACE_ROOT_DIR        "/home/omar/Desktop/Lovelace/"
+#define LOVELACE_ROOT_DIR        "/home/omar/Desktop/LovelaceEngine/"
 #define LOVELACE_VERSION_MAJOR   0
 #define LOVELACE_VERSION_MINOR   1
 #define                     DEBUG
