0e8b42c5d88095eb096097fff6ac6e4df4995b87
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-06-19 01:20:43 +0400

    Remove config.h

--- src/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define LOVELACE_ROOT_DIR        "/home/omar/Desktop/LovelaceEngine/"
-#define LOVELACE_VERSION_MAJOR   0
-#define LOVELACE_VERSION_MINOR   1
-#define                     DEBUG
