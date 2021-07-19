1cb38a94f0bf320019afd7ae09aa38f2d040904c
saccharineboi <77533355+saccharineboi@users.noreply.github.com>
2021-07-20 00:57:13 +0400

    Simplify log function

--- src/log.h
+++ src/log.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "config.h"
+
 #include <iostream>
 #include <chrono>
 #include <cstring>
@@ -8,16 +10,12 @@ namespace al
 {
         ////////////////////////////////////////////////////////////////////////////////
         template <typename... T>
-        inline void log(std::ostream& os, T&&... values)
+        inline void log(std::ostream& os, const char* file, int line, T&&... values)
         {
-                auto now = std::chrono::system_clock::now();
-                std::time_t current = std::chrono::system_clock::to_time_t(now);
-#define LOG_BUFFER_SIZE 32
-                char timeStr[LOG_BUFFER_SIZE];
-                strcpy(timeStr, ctime(&current));
-                timeStr[strlen(timeStr) - 1] = ']';
-                os << "[" << timeStr << '\t';
+#ifdef DEBUG
+                os << '[' << file << ":" << line << "] [" << __TIME__ << "] ";
                 (os << ... << values) << '\n';
+#endif
         }
 }
 
